bba6483d844d61fefd135a69291068a699520a79
w.a.paramithkavisha <paramithkavisha@gmail.com>
2026-03-09 00:13:29 +0530

    Initial setup - Restaurant Web Page

---
+++ server-side/src/main/java/com/example/dinustastyapi/DinusTastyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinustastyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinusTastyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinusTastyApiApplication.class, args);
+    }
+
+}
---
+++ server-side/src/test/java/com/example/dinustastyapi/DinusTastyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dinustastyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinusTastyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
